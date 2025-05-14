f530fdb1c0192edd2260d5c4c40ce78f17237877
ShashiCard91 <shashi.kushwaha@card91.io>
2025-05-14 18:23:29 +0530

    redis

--- src/main/java/org/example/grocery_app/config/RedisConfig.java
+++ src/main/java/org/example/grocery_app/config/RedisConfig.java
@@ -1,24 +1,43 @@
-package org.example.grocery_app.config;
+//package org.example.grocery_app.config;
+//
+//// package: org.example.grocery_app.config
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+//import org.springframework.data.redis.core.RedisTemplate;
+//
+//@Configuration
+//public class RedisConfig {
+//
+//    @Bean
+//    public JedisConnectionFactory jedisConnectionFactory() {
+//        return new JedisConnectionFactory();
+//    }
+//
+//    @Bean
+//    public RedisTemplate<String, String> redisTemplate() {
+//        RedisTemplate<String, String> template = new RedisTemplate<>();
+//        template.setConnectionFactory(jedisConnectionFactory());
+//        return template;
+//    }
+//}
 
-// package: org.example.grocery_app.config
+package org.example.grocery_app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Configuration
 public class RedisConfig {
 
-    @Bean
-    public JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
-    }
-
+    // This method defines the RedisTemplate bean,
+    // allowing Spring Boot's auto-configuration to handle the connection details
     @Bean
     public RedisTemplate<String, String> redisTemplate() {
-        RedisTemplate<String, String> template = new RedisTemplate<>();
-        template.setConnectionFactory(jedisConnectionFactory());
-        return template;
+        // Using StringRedisTemplate to simplify Redis interactions.
+        return new StringRedisTemplate();
     }
 }
