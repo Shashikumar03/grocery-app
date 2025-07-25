6750c8e5163701bd051f1d6c082fa0f0d8eb778a
Shashi  Kushwaha <shashikushwaha@MacBookAir.bbrouter>
2025-07-26 02:31:44 +0530

    all

--- src/main/java/org/example/grocery_app/service/FeeService.java
+++ src/main/java/org/example/grocery_app/service/FeeService.java
@@ -1,8 +1,10 @@
 package org.example.grocery_app.service;
 
+import org.example.grocery_app.entities.FeeTable;
+
 public interface FeeService {
 
     void  createFees();
 
-    int getDeliveryFees(Long id);
+    FeeTable getDeliveryFees(Long id);
 }
