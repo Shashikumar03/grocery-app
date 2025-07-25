26995394582bd8799009eb082210f786dc5dc997
Shashi  Kushwaha <shashikushwaha@MacBookAir.bbrouter>
2025-07-25 21:37:16 +0530

    all

--- src/main/java/org/example/grocery_app/repository/FeeTableRepository.java
+++ src/main/java/org/example/grocery_app/repository/FeeTableRepository.java
@@ -1,8 +1,7 @@
 package org.example.grocery_app.repository;
 
-//import org.example.grocery_app.entities.Fee;
 import org.example.grocery_app.entities.FeeTable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FeeRepository extends JpaRepository<FeeTable, Long> {
+public interface FeeTableRepository extends JpaRepository<FeeTable, Long> {
 }
---
+++ src/main/java/org/example/grocery_app/service/FeeService.java
@@ -0,0 +1,8 @@
+package org.example.grocery_app.service;
+
+public interface FeeService {
+
+    void  createFees();
+
+    int getDeliveryFees(Long id);
+}
